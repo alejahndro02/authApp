c0de6813359b133e7bc743e96f52c95b612ddb51
alejahndro02 <alejahndro02@gmail.com>
2022-03-26 19:29:23 -0600

    se actualizo el modulo con la importcion del componente de dashboard

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule               } from '@angular/core';
+import { CommonModule           } from '@angular/common';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
+import { DashboardComponent     } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     ProtectedRoutingModule
