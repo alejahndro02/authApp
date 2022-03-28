05da50877caccf7c550673ff42f273b597e48a24
alejahndro02 <alejahndro02@gmail.com>
2022-03-28 13:47:56 -0600

    Se agregaron las referencias para el fomulario reactivo entre el html y ts

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup= this.fb.group({
+    nameUser: ['', [Validators.required]],
+    email   : ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-
+  registro(){
+    console.log(this.registerForm.valid);
+    console.log(this.registerForm.value);
+    console.log('registrado');
+    
+  }
+  
+  
+  
+  
 }
