4320bb0d478eb6947e3762f2a52685c20c202094
alejahndro02 <alejahndro02@gmail.com>
2022-03-28 20:03:28 -0600

    se inyecta el authService y se crea un geter que retorna la propiedad usuario proveniente del servicio

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component   } from '@angular/core';
+import { Router      } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +11,15 @@ import { Router } from '@angular/router';
     }`
   ]
 })
-export class DashboardComponent implements OnInit {
-
-  constructor(private router:Router) { }
-
-  ngOnInit(): void {
+export class DashboardComponent{
+  get usuario(){
+    return this.authService.usuario
   }
+
+  constructor(private router:Router,
+              private authService: AuthService) { }
+
+
  logOut(){
   this.router.navigateByUrl('/auth')
  }
