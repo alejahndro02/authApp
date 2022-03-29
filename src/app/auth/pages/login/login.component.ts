ca6a183e691f37c7bf42943007152404998a0a55
alejahndro02 <alejahndro02@gmail.com>
2022-03-28 18:47:36 -0600

    se importa el authService y se ejecuta el metodo login pasandole los parametros email y password provenientes del html

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder,
          FormGroup, 
          Validators   } from '@angular/forms';
 import { Router       } from '@angular/router';
+import { AuthService  } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -17,9 +18,19 @@ export class LoginComponent {
     password:['', [Validators.required, Validators.minLength(6)]]
   })
 
-  constructor(private fb: FormBuilder, private router: Router) { }
+  constructor(private fb: FormBuilder, 
+              private router: Router,
+              private authService: AuthService) { }
 
   login(){
-    this.router.navigateByUrl('/dashboard')
+    console.log(this.loginForm.value);
+    const {email, password} = this.loginForm.value
+    this.authService.login(email,password)
+    .subscribe( resp => {
+      console.log(resp);
+      
+    })
+
+    // this.router.navigateByUrl('/dashboard')
   }
 }
