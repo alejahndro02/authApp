20a40de2626316066ac9624448a474deca8e5923
alejahndro02 <alejahndro02@gmail.com>
2022-03-28 18:44:30 -0600

    Se declara una propiedad dbUrlApi que almacena la ruta proveniente de los evironments y dentro del metodo login se hace una peticion post al api

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient   } from '@angular/common/http';
+import { Injectable   } from '@angular/core';
+
+import { environment  } from 'src/environments/environment';
+import { AuthResponse } from '../interfaces/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private dbUrlApi: string= environment.dbUrl
+  constructor(private http: HttpClient) { }
+
+  login(email:string, password:string){
+    const urlApp=`${this.dbUrlApi}/auth`
+    const body = {email, password} 
+    
+    return this.http.post<AuthResponse>(urlApp, body)
+  }
 }
