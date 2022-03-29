6ef725f44dd10574bca84dee58d08f08fca166f6
alejahndro02 <alejahndro02@gmail.com>
2022-03-28 20:01:06 -0600

    se importo la interface de ususario y se creo una propiedad y un geter de tipo usuario, y en el metodo ost se pasa por el pipe, y deto el tap para mostrar evaluar si la respuesta es ok manda un mensaje personalizado, y cacha el error mediante catchError

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,13 @@
 import { HttpClient   } from '@angular/common/http';
 import { Injectable   } from '@angular/core';
+import { catchError, 
+         map, 
+         of, 
+         tap          } from 'rxjs';
 
 import { environment  } from 'src/environments/environment';
-import { AuthResponse } from '../interfaces/auth';
+import { AuthResponse, 
+         Usuario      } from '../interfaces/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +15,32 @@ import { AuthResponse } from '../interfaces/auth';
 export class AuthService {
 
   private dbUrlApi: string= environment.dbUrl
+  private _usuario!:Usuario; 
+
+  get usuario(){
+    return {...this._usuario}
+  }
+
   constructor(private http: HttpClient) { }
 
   login(email:string, password:string){
+
     const urlApp=`${this.dbUrlApi}/auth`
     const body = {email, password} 
     
     return this.http.post<AuthResponse>(urlApp, body)
+    .pipe(
+      tap(response=>{
+        console.log(response)
+        if(response.ok){
+          this._usuario={
+            name:response.nameUser!,
+            uid:response.uid!
+          }
+        }
+      }),
+      map(res => res.ok),
+      catchError(err => of(false))
+    )
   }
 }
