faca6df5298c2e8ea6cce8922870c9f94463c8e2
alejahndro02 <alejahndro02@gmail.com>
2022-03-30 10:20:10 -0600

    se declaro el metodo de registro donde se ejecuta el metodo registro proveniente del authService y si este es valido direcciona al dashboard sino muestra un mensaje de error

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder,
          FormGroup, 
          Validators   } from '@angular/forms';
 import { Router       } from '@angular/router';
+import { AuthService  } from 'src/app/auth/services/auth.service';
+import   Swal           from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -17,12 +19,26 @@ export class RegisterComponent implements OnInit {
     email   : ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   })
-  constructor(private fb: FormBuilder,private router: Router) { }
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
   }
   registro(){
-    this.router.navigateByUrl('/auth')
+    const { nameUser, email, password} = this.registerForm.value
+    
+    this.authService.registro(nameUser,email,password)
+    .subscribe( valido => {
+      console.log(valido );
+      if(valido===true){
+        this.router.navigateByUrl('/dashboard')
+      }else{
+        console.log('error');
+        Swal.fire('Error', valido, 'error')
+      }
+    })
+
   }
   
 }
